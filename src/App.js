734ca7208ccf03759ff08f12589aa5b411bb8918
Hamzah-syed <50793209+Hamzah-syed@users.noreply.github.com>
2021-01-25 00:09:36 +0500

    project completed

--- src/App.js
+++ src/App.js
@@ -1,108 +1,165 @@
-import React, { useState } from 'react';
-import './App.css';
-import Slider from './Slider'
-import SidebarItem from './SidebarItem'
+import React, { useState, useRef, useEffect } from "react";
+import "./App.css";
+import Slider from "./Slider";
+import SidebarItem from "./SidebarItem";
+import Img from "./img/profile.jpg";
+import UploadImage from "./uploadImage";
 
 const DEFAULT_OPTIONS = [
   {
-    name: 'Brightness',
-    property: 'brightness',
+    name: "Brightness",
+    property: "brightness",
     value: 100,
     range: {
       min: 0,
-      max: 200
+      max: 200,
     },
-    unit: '%'
+    unit: "%",
   },
   {
-    name: 'Contrast',
-    property: 'contrast',
+    name: "Contrast",
+    property: "contrast",
     value: 100,
     range: {
       min: 0,
-      max: 200
+      max: 200,
     },
-    unit: '%'
+    unit: "%",
   },
   {
-    name: 'Saturation',
-    property: 'saturate',
+    name: "Saturation",
+    property: "saturate",
     value: 100,
     range: {
       min: 0,
-      max: 200
+      max: 200,
     },
-    unit: '%'
+    unit: "%",
   },
   {
-    name: 'Grayscale',
-    property: 'grayscale',
+    name: "Grayscale",
+    property: "grayscale",
     value: 0,
     range: {
       min: 0,
-      max: 100
+      max: 100,
     },
-    unit: '%'
+    unit: "%",
   },
   {
-    name: 'Sepia',
-    property: 'sepia',
+    name: "Sepia",
+    property: "sepia",
     value: 0,
     range: {
       min: 0,
-      max: 100
+      max: 100,
     },
-    unit: '%'
+    unit: "%",
   },
   {
-    name: 'Hue Rotate',
-    property: 'hue-rotate',
+    name: "Hue Rotate",
+    property: "hue-rotate",
     value: 0,
     range: {
       min: 0,
-      max: 360
+      max: 360,
     },
-    unit: 'deg'
+    unit: "deg",
   },
   {
-    name: 'Blur',
-    property: 'blur',
+    name: "Blur",
+    property: "blur",
     value: 0,
     range: {
       min: 0,
-      max: 20
+      max: 20,
     },
-    unit: 'px'
-  }
-]
+    unit: "px",
+  },
+];
 
 function App() {
-  const [selectedOptionIndex, setSelectedOptionIndex] = useState(0)
-  const [options, setOptions] = useState(DEFAULT_OPTIONS)
-  const selectedOption = options[selectedOptionIndex]
-
-  function handleSliderChange({ target }) {
-    setOptions(prevOptions => {
-      return prevOptions.map((option, index) => {
-        if (index !== selectedOptionIndex) return option
-        return { ...option, value: target.value }
-      })
-    })
-  }
+  const [selectedOptionIndex, setSelectedOptionIndex] = useState(0);
+  const [options, setOptions] = useState(DEFAULT_OPTIONS);
+  //used in upload image state
+  const [selectedFile, setSelectedFile] = useState();
+  const [preview, setPreview] = useState();
+  const selectedOption = options[selectedOptionIndex];
+  const canvasRef = useRef(null);
 
   function getImageStyle() {
-    const filters = options.map(option => {
-      return `${option.property}(${option.value}${option.unit})`
-    })
+    const filters = options.map((option) => {
+      return `${option.property}(${option.value}${option.unit})`;
+    });
 
-    return { filter: filters.join(' ') }
+    return { filter: filters.join(" ") };
   }
 
-  console.log(getImageStyle())
+  console.log(getImageStyle());
+
+  const canvasToImg = (_) => {
+    try {
+      let canvas = canvasRef.current;
+      let context = canvas.getContext("2d");
+      let img = new Image();
+      //edited from
+      img.crossOrigin = "anonymous";
+      img.src = Img;
+      //edited end
+      // context.fillRect(100, 100, 100, 100);
+      canvas.width = 700;
+      canvas.height = 700;
+
+      context.filter = getImageStyle().filter;
+      img.addEventListener("load", function () {
+        context.drawImage(img, 0, 0, 700, 700);
+        let tagA = document.createElement("a");
+        document.body.appendChild(tagA);
+        tagA.href = canvas.toDataURL();
+        tagA.download = "canvas-image.png";
+        tagA.click();
+        document.body.removeChild(tagA);
+      });
+      // context.drawImage(img, 0, 0, 100, 100);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  function handleSliderChange({ target }) {
+    setOptions((prevOptions) => {
+      return prevOptions.map((option, index) => {
+        if (index !== selectedOptionIndex) return option;
+        return { ...option, value: target.value };
+      });
+    });
+  }
 
   return (
     <div className="container">
-      <div className="main-image" style={getImageStyle()} />
+      <div className="uploadImageContainer">
+        <UploadImage
+          selectedFile={selectedFile}
+          setSelectedFile={setSelectedFile}
+          preview={preview}
+          setPreview={setPreview}
+        />
+      </div>
+      {selectedFile && (
+        <img className="main-image" src={preview} style={getImageStyle()} />
+      )}
+      {selectedFile && (
+        <button onClick={canvasToImg} style={{ marginLeft: 20 }}>
+          Download
+        </button>
+      )}
+
+      <Slider
+        min={selectedOption.range.min}
+        max={selectedOption.range.max}
+        value={selectedOption.value}
+        handleChange={handleSliderChange}
+      />
       <div className="sidebar">
         {options.map((option, index) => {
           return (
@@ -112,17 +169,14 @@ function App() {
               active={index === selectedOptionIndex}
               handleClick={() => setSelectedOptionIndex(index)}
             />
-          )
+          );
         })}
       </div>
-      <Slider
-        min={selectedOption.range.min}
-        max={selectedOption.range.max}
-        value={selectedOption.value}
-        handleChange={handleSliderChange}
-      />
+      <div style={{ display: "none" }}>
+        <canvas ref={canvasRef} />
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
