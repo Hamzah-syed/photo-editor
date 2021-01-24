b86ce6407982cdc37c155d74821d8ac8d2d6d2de
Hamzah-syed <50793209+Hamzah-syed@users.noreply.github.com>
2021-01-25 00:21:07 +0500

    fixed warning

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef } from "react";
 import "./App.css";
 import Slider from "./Slider";
 import SidebarItem from "./SidebarItem";
